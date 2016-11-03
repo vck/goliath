bffbe045d526f9b06e6d915e597d8833171f9ac7
vickydasta <vickydasta@gmail.com>
2016-11-03 13:35:06 +0700

    initial commit

---
+++ goliath/interpreter/intrprtr.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import re
+
+
+html = {"header1":"<h1>",
+        "header2":"<h2>",
+        "header3":"<h3>",
+        "header4":"<h4>",
+        "bold":"<strong>"
+}
+
+
+md_re = {"header1":u"[\#"]",
+         "header2":u"[\##]",
+         "header3":u"[\###]"
+}
+
+<F12>
+
