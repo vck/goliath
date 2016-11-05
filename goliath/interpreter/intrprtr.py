81c787e947f9f7edcc3b17f68787b411c8a88eef
vickydasta <vickydasta@gmail.com>
2016-11-05 11:34:13 +0700

    goodone m8

--- goliath/interpreter/intrprtr.py
+++ goliath/interpreter/intrprtr.py
@@ -18,17 +18,6 @@ md_re = {"header": u'[\#]'}
 
 
 def symbol_feeder(content):
-    """bruteforce each regex in known
-    markdown tags
-    """
-    for reg in md_re.values():
-        result = re.findall(reg, content)
-        if result > 0:
-            parsed_symbol = {result: content.strip(result[0])}
-    return parsed_symbol
-
-
-def symbol_feederx(content):
     """docstring for symbol_feederx
     """
     symbols = re.findall(md_re["header"], content)
@@ -63,10 +52,34 @@ def markpreter(symbol):
         return html_tag.format(symbol_target.values()[0])
 
 
+def markpreterx(content):
+    if symbol_feeder(content):
+        # check if no errors occur
+        symbol_target = symbol_feeder(content)
+        tag = symbol_target[0]
+        content = symbol_target[1]
+        if "#" in tag:
+            if len(tag) is 1:
+                html_tag = html["header1"]
+            elif len(tag) is 2:
+                html_tag = html["header2"]
+            elif len(tag) is 3:
+                html_tag = html["header3"]
+            elif len(tag) is 4:
+                html_tag = html["header4"]
+            elif len(tag) is 5:
+                html_tag = html["header5"]
+            elif len(tag) is 6:
+                html_tag = html["header6"]
+        return html_tag.format(content)
+
+
 def runtest():
     test = ["#h1", "##2", "###3", "####4"]
-    print "-----------------"
+
     for tag in test:
-        print tag, symbol_feederx(tag)
+        print tag, " --> ", markpreterx(tag)
+
+
 if __name__ == '__main__':
     runtest()
